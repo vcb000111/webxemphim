198e2aa54d4838703877a0d161fdd2c9d3650483
vcb000111 <vcb000111@gmail.com>
2020-03-09 19:58:39 +0700

    Update Link.php

--- app/Link.php
+++ app/Link.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Link extends Model
 {
-    //
+    //hihi
     protected $table='links';
     protected $fillable=['movie_id','link1','link2','link3','link4','link5'];
     public $timestamps=false;
